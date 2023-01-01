fff246cf9de2c7a7a03635d1de64709534df42e5
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-01 14:57:51 +0700

    feat: initial commit

---
+++ src/Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using SampSharp.Core;
+using SampSharp.Entities;
+
+namespace Server
+{
+	public class Program
+	{
+		private static void Main()
+		{
+			new GameModeBuilder()
+				.UseEcs<Startup>()
+				.Run();
+		}
+	}
+}
---
+++ src/Server/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SampSharp.Entities;
+using SampSharp.Entities.SAMP;
+
+namespace Server
+{
+	public class Startup : IStartup
+	{
+		public void Configure(IServiceCollection services)
+		{
+			services
+				.AddSystemsInAssembly();
+		}
+
+		public void Configure(IEcsBuilder builder)
+		{
+			builder.EnableSampEvents() // Enable all stock SA-MP callbacks as events which can be listened to by systems.
+				.EnablePlayerCommands(); // Enable player commands being loaded in systems.
+		}
+	}
+}
