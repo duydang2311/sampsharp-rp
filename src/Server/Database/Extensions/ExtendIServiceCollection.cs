46a1d5857ced691c6c63687e99c9753acbd9365e
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-02 22:37:17 +0700

    refactor(Database): add port parameter

--- src/Server/Database/Extensions/ExtendIServiceCollection.cs
+++ src/Server/Database/Extensions/ExtendIServiceCollection.cs
@@ -1,3 +1,4 @@
+using dotenv.net;
 using Microsoft.EntityFrameworkCore;
 using Server.Database;
 
@@ -7,10 +8,12 @@ public static class ExtendIServiceCollection
 {
 	public static IServiceCollection AddDatabase(this IServiceCollection self)
 	{
+		DotEnv.Load(options: new DotEnvOptions(overwriteExistingVars: false));
 		var host = Environment.GetEnvironmentVariable("host");
 		var username = Environment.GetEnvironmentVariable("username");
 		var password = Environment.GetEnvironmentVariable("password");
 		var database = Environment.GetEnvironmentVariable("database");
+		var port = Environment.GetEnvironmentVariable("port") ?? "5432";
 		if (host is null
 		|| username is null
 		|| password is null
@@ -20,7 +23,7 @@ public static class ExtendIServiceCollection
 		}
 		return self.AddDbContext<IServerDbContext, ServerDbContext>((optionsBuilder) =>
 		{
-			optionsBuilder.UseNpgsql($"Host={host};Username={username};Password={password};Database={database}");
+			optionsBuilder.UseNpgsql($"Host={host};Username={username};Password={password};Database={database};Port={port}");
 			optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 		});
 	}
