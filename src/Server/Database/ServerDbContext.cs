fa8fce9622179327396266edbf45fcf12eeaac32
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-04 02:27:25 +0700

    fix(ServerDbContext): initialize property values

--- src/Server/Database/ServerDbContext.cs
+++ src/Server/Database/ServerDbContext.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using Server.Character.Models;
 
 namespace Server.Database;
 
-public sealed class ServerDbContext : DbContext, IServerDbContext
+public sealed class ServerDbContext : DbContext
 {
+	public DbSet<CharacterModel> CharacterModels => Set<CharacterModel>();
+	public DbSet<CharacterAuthModel> CharacterAuthModels => Set<CharacterAuthModel>();
+	public DbSet<CharacterSpawnModel> CharacterSpawnModels => Set<CharacterSpawnModel>();
+
 	public ServerDbContext(DbContextOptions options) : base(options) { }
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 }
