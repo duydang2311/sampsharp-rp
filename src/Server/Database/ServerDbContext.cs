9d90353e7b5a79d271633216a6a401ddf324d582
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-05 02:52:37 +0700

    feat: add property dbsets; build property model

--- src/Server/Database/ServerDbContext.cs
+++ src/Server/Database/ServerDbContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Server.Account.Models;
 using Server.Character.Models;
 using Server.Door.Models;
-
+using Server.Property.Models;
 namespace Server.Database;
 
 public sealed class ServerDbContext : DbContext
@@ -10,6 +10,8 @@ public sealed class ServerDbContext : DbContext
 	public DbSet<CharacterModel> Characters => Set<CharacterModel>();
 	public DbSet<AccountModel> Accounts => Set<AccountModel>();
 	public DbSet<DoorModel> Doors => Set<DoorModel>();
+	public DbSet<PropertyModel> Properties => Set<PropertyModel>();
+	public DbSet<PropertyPointModel> PropertyPoints => Set<PropertyPointModel>();
 
 	public ServerDbContext() : base() { }
 	public ServerDbContext(DbContextOptions options) : base(options) { }
@@ -23,5 +25,6 @@ public sealed class ServerDbContext : DbContext
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
 		modelBuilder.BuildDoor();
+		modelBuilder.BuildProperty();
 	}
 }
