3c67b3b735ec981aae0ab661f23733510117f0b0
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-06 20:00:41 +0700

    refactor: delete custom command service

--- src/Server/Chat/Services/ICustomPlayerCommandService.cs
+++
@@ -1,9 +0,0 @@
-using SampSharp.Entities;
-using SampSharp.Entities.SAMP.Commands;
-
-namespace Server.Chat.Services;
-
-public interface ICustomPlayerCommandService
-{
-	InvokeResult Invoke(IServiceProvider services, EntityId player, string inputText);
-}
