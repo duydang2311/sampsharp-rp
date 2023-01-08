a9577579a5a629236c0540d1b4b36b5f4f39b122
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-09 01:35:59 +0700

    feat: register I18n services; configure options

---
+++ src/Server/I18n/Extensions/ExtendIServiceCollection.cs
@@ -0,0 +1,19 @@
+using Server.I18n.Globalization.Services;
+using Server.I18n.Localization.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class ExtendIServiceCollection
+{
+	public static IServiceCollection WithI18n(this IServiceCollection self, Action<IGlobalCultureServiceOptions, ITextLocalizerServiceOptions> configI18n)
+	{
+		var globalCultureOptions = new GlobalCultureServiceOptions();
+		var textLocalizerOptions = new TextLocalizerServiceOptions();
+		configI18n(globalCultureOptions, textLocalizerOptions);
+		return self
+			.AddSingleton<IGlobalCultureServiceOptions>(provider => globalCultureOptions)
+			.AddSingleton<ITextLocalizerServiceOptions>(provider => textLocalizerOptions)
+			.AddSingleton<IGlobalCultureService, GlobalCultureService>()
+			.AddSingleton<ITextLocalizerService, TextLocalizerService>();
+	}
+}
