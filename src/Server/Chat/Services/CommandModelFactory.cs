3c04346a4e26cac80012047092ed91038049b204
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-11 01:50:10 +0700

    feat: add Create overloads

--- src/Server/Chat/Services/CommandModelFactory.cs
+++ src/Server/Chat/Services/CommandModelFactory.cs
@@ -5,12 +5,23 @@ namespace Server.Chat.Services;
 
 public sealed class CommandModelFactory : ICommandModelFactory
 {
+	public CommandModel Create()
+	{
+		return new CommandModel();
+	}
+
 	public CommandModel Create(string name)
 	{
 		return new CommandModel { Name = name };
 	}
+
 	public CommandModel Create(string name, PermissionLevel permissionLevel)
 	{
 		return new CommandModel { Name = name, PermissionLevel = permissionLevel };
+	}
+
+	public CommandModel Create(string name, PermissionLevel permissionLevel, Delegate @delegate)
+	{
+		return new CommandModel { Name = name, PermissionLevel = permissionLevel, Delegate = @delegate };
 	}
 }
--- src/Server/Chat/Services/ICommandModelFactory.cs
+++ src/Server/Chat/Services/ICommandModelFactory.cs
@@ -5,6 +5,7 @@ namespace Server.Chat.Services;
 
 public interface ICommandModelFactory
 {
+	CommandModel Create();
 	CommandModel Create(string name);
 	CommandModel Create(string name, PermissionLevel permissionLevel);
 }
