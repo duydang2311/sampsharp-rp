b474ac4edcdd56b814fd2ee3f9bfd35abb765c10
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-10 23:54:10 +0700

    feat: add command model

---
+++ src/Server/Chat/Models/CommandModel.cs
@@ -0,0 +1,9 @@
+using Server.Chat.Components;
+
+namespace Server.Chat.Models;
+
+public sealed class CommandModel
+{
+	public string Name { get; set; } = string.Empty;
+	public PermissionLevel PermissionLevel { get; set; }
+}
