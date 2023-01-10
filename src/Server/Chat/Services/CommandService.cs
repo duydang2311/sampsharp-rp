73befbfd4021ea843a2fdaf6488cdef86370fdd8
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-11 01:50:32 +0700

    refactor!: register command using CommandModel

--- src/Server/Chat/Services/CommandService.cs
+++ src/Server/Chat/Services/CommandService.cs
@@ -1,11 +1,17 @@
-using SampSharp.Entities.SAMP;
+using Server.Chat.Models;
 
 namespace Server.Chat.Services;
 
 public sealed class CommandService : ICommandService
 {
-	private readonly IDictionary<string, Delegate> commandDict = new Dictionary<string, Delegate>();
+	private readonly ICommandModelFactory commandModelFactory;
+	private readonly IDictionary<string, CommandModel> commandDict = new Dictionary<string, CommandModel>();
 	private readonly IDictionary<string, Delegate> helperDict = new Dictionary<string, Delegate>();
+
+	public CommandService(ICommandModelFactory commandModelFactory)
+	{
+		this.commandModelFactory = commandModelFactory;
+	}
 
 	public bool HasCommand(string command)
 	{
@@ -17,14 +23,18 @@ public sealed class CommandService : ICommandService
 		return helperDict.ContainsKey(command);
 	}
 
-	public Delegate GetCommandDelegate(string command)
+	public CommandModel GetCommandModel(string command)
 	{
 		return commandDict[command];
 	}
 
 	public object? InvokeCommand(string command, object?[]? arguments)
 	{
-		var @delegate = commandDict[command];
+		var @delegate = commandDict[command].Delegate;
+		if (@delegate is null)
+		{
+			return default;
+		}
 		var method = @delegate.Method;
 		return method.Invoke(@delegate.Target, arguments);
 	}
@@ -41,88 +51,10 @@ public sealed class CommandService : ICommandService
 		helperDict.Add(command, handler);
 	}
 
-	public void RegisterCommand(string command, Delegate handler)
+	public void RegisterCommand(Action<CommandModel> modelAction)
 	{
-		commandDict.Add(command, handler);
-	}
-
-	public void RegisterCommand(string command, Action<Player> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1>(string command, Action<Player, T1> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2>(string command, Action<Player, T1, T2> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3>(string command, Action<Player, T1, T2, T3> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4>(string command, Action<Player, T1, T2, T3, T4> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4, T5>(string command, Action<Player, T1, T2, T3, T4, T5> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4, T5, T6>(string command, Action<Player, T1, T2, T3, T4, T5, T6> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4, T5, T6, T7>(string command, Action<Player, T1, T2, T3, T4, T5, T6, T7> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand(string command, Func<Player> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1>(string command, Func<Player, T1, Task> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2>(string command, Func<Player, T1, T2, Task> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3>(string command, Func<Player, T1, T2, T3, Task> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4>(string command, Func<Player, T1, T2, T3, T4, Task> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4, T5>(string command, Func<Player, T1, T2, T3, T4, T5, Task> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4, T5, T6>(string command, Func<Player, T1, T2, T3, T4, T5, T6, Task> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
-	}
-
-	public void RegisterCommand<T1, T2, T3, T4, T5, T6, T7>(string command, Func<Player, T1, T2, T3, T4, T5, T6, T7, Task> handler)
-	{
-		RegisterCommand(command, handler as Delegate);
+		var model = commandModelFactory.Create();
+		modelAction(model);
+		commandDict.Add(model.Name, model);
 	}
 }
