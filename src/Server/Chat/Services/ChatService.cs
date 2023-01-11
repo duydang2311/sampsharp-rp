0dd548bf7f39e6864024f281e130d601ffa57358
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-11 20:08:15 +0700

    refactor!: remove i18n from chat service; use factory instead

--- src/Server/Chat/Services/ChatService.cs
+++ src/Server/Chat/Services/ChatService.cs
@@ -1,23 +1,16 @@
-using System.Linq.Expressions;
 using System.Text;
 using SampSharp.Entities.SAMP;
 using Server.Chat.Models;
-using Server.I18n.Localization.Models;
-using Server.I18n.Localization.Services;
 
 namespace Server.Chat.Services;
 
 public sealed class ChatService : IChatService
 {
 	private readonly IChatMessageModelFactory factory;
-	private readonly ITextNameIdentifierService identifierService;
-	private readonly ITextLocalizerService textLocalizerService;
 
-	public ChatService(IChatMessageModelFactory factory, ITextNameIdentifierService identifierService, ITextLocalizerService textLocalizerService)
+	public ChatService(IChatMessageModelFactory factory)
 	{
 		this.factory = factory;
-		this.identifierService = identifierService;
-		this.textLocalizerService = textLocalizerService;
 	}
 
 	private static void SendMessage(Player player, Color color, string message)
@@ -46,32 +39,9 @@ public sealed class ChatService : IChatService
 		var stringBuilder = new StringBuilder(144);
 		foreach (var model in models)
 		{
-			stringBuilder.AppendFormat("{{0}}{1}", model.Color.ToString(), model.Text);
+			stringBuilder.AppendFormat("{{0}}{1} ", model.Color.ToString(), model.Text);
 		}
+		stringBuilder.Remove(stringBuilder.Length - 1, 1);
 		SendMessage(player, Color.White, stringBuilder.ToString());
-	}
-
-	public void SendMessage(Player player, Expression<Func<ITextNameFakeModel, object>> textIdentifier)
-	{
-		var name = identifierService.Identify(textIdentifier);
-		SendMessage(player, Color.White, textLocalizerService.Get(name));
-	}
-
-	public void SendMessage(Player player, Expression<Func<ITextNameFakeModel, object>> textIdentifier, params object[] args)
-	{
-		var name = identifierService.Identify(textIdentifier);
-		SendMessage(player, Color.White, textLocalizerService.Get(name, args));
-	}
-
-	public void SendMessage(Player player, Color color, Expression<Func<ITextNameFakeModel, object>> textIdentifier)
-	{
-		var name = identifierService.Identify(textIdentifier);
-		SendMessage(player, color, textLocalizerService.Get(name));
-	}
-
-	public void SendMessage(Player player, Color color, Expression<Func<ITextNameFakeModel, object>> textIdentifier, params object[] args)
-	{
-		var name = identifierService.Identify(textIdentifier);
-		SendMessage(player, color, textLocalizerService.Get(name, args));
 	}
 }
