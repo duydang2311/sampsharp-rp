9ad38662bdbea288c6f3a08d853c9b26242411f5
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-11 20:02:33 +0700

    feat: add Create overloads with integrated I18n

--- src/Server/Chat/Services/IChatMessageModelFactory.cs
+++ src/Server/Chat/Services/IChatMessageModelFactory.cs
@@ -1,10 +1,16 @@
+using System.Linq.Expressions;
 using SampSharp.Entities.SAMP;
 using Server.Chat.Models;
+using Server.I18n.Localization.Models;
 
 namespace Server.Chat.Services;
 
 public interface IChatMessageModelFactory
 {
-	ChatMessageModel Create(Color color, string text);
+	ChatMessageModel Create(Expression<Func<ITextNameFakeModel, object>> identifier);
+	ChatMessageModel Create(Color color, Expression<Func<ITextNameFakeModel, object>> identifier);
+	ChatMessageModel Create(Expression<Func<ITextNameFakeModel, object>> identifier, params object[] args);
+	ChatMessageModel Create(Color color, Expression<Func<ITextNameFakeModel, object>> identifier, params object[] args);
 	ChatMessageModel Create(string text);
+	ChatMessageModel Create(Color color, string text);
 }
