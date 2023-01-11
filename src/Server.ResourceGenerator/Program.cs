f3385ff56b6c57becc36515d7c61a1d9b4f5ee19
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-11 23:18:41 +0700

    feat: generate unidecoded Vietnamese resource on run

---
+++ src/Server.ResourceGenerator/Program.cs
@@ -0,0 +1,11 @@
+﻿using Server.ResourceGenerator.Text;
+
+namespace Server.ResourceGenerator;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		VietnameseUnidecodedGenerator.Generate();
+	}
+}
