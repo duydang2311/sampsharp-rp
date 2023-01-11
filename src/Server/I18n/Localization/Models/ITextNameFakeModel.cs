75444d0ea5ad9acbe834f5b667abd14be6bdbdc0
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-11 20:10:59 +0700

    feat: add Badge_System text

--- src/Server/I18n/Localization/Models/ITextNameFakeModel.cs
+++ src/Server/I18n/Localization/Models/ITextNameFakeModel.cs
@@ -2,6 +2,7 @@ namespace Server.I18n.Localization.Models;
 
 public interface ITextNameFakeModel
 {
+	object Badge_System { get; set; }
 	object Command_NotFound { get; set; }
 	object Command_Denied { get; set; }
 }
