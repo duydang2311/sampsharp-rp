9da921b170c5b0ff7e3af440c319f5f2a18165a4
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-14 04:59:43 +0700

    refactor: remove manual system instantiation on startup

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -1,56 +1,33 @@
 using System.Globalization;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using SampSharp.Entities;
 using SampSharp.Entities.SAMP;
-using Server.I18N.Localization.Services;
 
 namespace Server
 {
-	public class Startup : IStartup
-	{
-		public void Configure(IServiceCollection services)
-		{
-			services
-				.WithDatabase()
-				.WithLogging()
-				.WithChat()
-				.WithI18N((globalOptions, textLocalizerOptions) =>
-				{
-					globalOptions.AddCulture("vi");
-					globalOptions.AddCulture("vi-VN");
-					textLocalizerOptions.BaseName = "Server.Resources.Text";
-					textLocalizerOptions.DefaultCulture = CultureInfo.GetCultureInfo("vi");
-				})
-				.WithSAMP();
+    public class Startup : IStartup
+    {
+        public void Configure(IServiceCollection services)
+        {
+            services
+                .WithDatabase()
+                .WithLogging()
+                .WithChat()
+                .WithI18N((globalOptions, textLocalizerOptions) =>
+                {
+                    globalOptions.AddCulture("vi");
+                    globalOptions.AddCulture("vi-VN");
+                    textLocalizerOptions.BaseName = "Server.Resources.Text";
+                    textLocalizerOptions.DefaultCulture = CultureInfo.GetCultureInfo("vi");
+                })
+                .WithSAMP();
+        }
 
-			var provider = services.BuildServiceProvider();
-			var logger = provider.GetRequiredService<ILogger<Startup>>();
-			foreach (var service in services)
-			{
-				if (service.Lifetime != ServiceLifetime.Singleton)
-				{
-					continue;
-				}
-				var serviceType = service.ServiceType;
-				var implType = service.ImplementationType;
-				if (serviceType.Namespace!.StartsWith("Server")
-				&& serviceType.IsAssignableTo(typeof(ISystem))
-				|| (implType is not null
-					&& implType.Namespace!.StartsWith("Server")
-					&& implType.IsAssignableTo(typeof(ISystem))))
-				{
-					logger.LogInformation("Enabled system {System}", serviceType.FullName);
-					provider.GetRequiredService(serviceType);
-				}
-			}
-		}
-
-		public void Configure(IEcsBuilder builder)
-		{
-			builder
-				.UseChatMiddlewares()
-				.EnableSampEvents();
-		}
-	}
+        public void Configure(IEcsBuilder builder)
+        {
+            builder
+                .UseChatMiddlewares()
+                .EnableSampEvents();
+        }
+    }
 }
