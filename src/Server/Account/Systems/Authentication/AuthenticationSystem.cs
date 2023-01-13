f896c5ccc44320db51754c10d406ce4186011061
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-14 01:28:54 +0700

    feat: invoke authenticated event in auth system

--- src/Server/Account/Systems/Authentication/AuthenticationSystem.cs
+++ src/Server/Account/Systems/Authentication/AuthenticationSystem.cs
@@ -12,36 +12,29 @@ namespace Server.Account.Systems.Authentication;
 public sealed class AuthenticationSytem : ISystem
 {
     private readonly IDbContextFactory<ServerDbContext> contextFactory;
-    private readonly ILoginSystem loginSystem;
-    private readonly ISignUpSystem signUpSystem;
     private readonly IChatService chatService;
-    public AuthenticationSytem(IDbContextFactory<ServerDbContext> contextFactory, ILoginSystem loginSystem, ISignUpSystem signUpSystem, IChatService chatService)
+    private readonly IAuthenticatedEvent authenticatedEvent;
+
+    public AuthenticationSytem(IDbContextFactory<ServerDbContext> contextFactory, IChatService chatService,
+        IAuthenticatedEvent authenticatedEvent)
     {
         this.contextFactory = contextFactory;
-        this.loginSystem = loginSystem;
-        this.signUpSystem = signUpSystem;
         this.chatService = chatService;
+        this.authenticatedEvent = authenticatedEvent;
     }
 
     [Event]
     private void OnPlayerConnect(Player player)
     {
-        chatService.SendMessage(player, (factory) => factory.Create("Đang tải, vui lòng đợi trong giây lát..."));
+        chatService.SendMessage(player, f => f.Create("Đang tải, vui lòng đợi trong giây lát..."));
     }
 
     [Event]
     private async Task OnPlayerRequestClass(Player player, int classid)
     {
-        await using var context = await contextFactory.CreateDbContextAsync();
-        var exist = await context.Characters.AnyAsync(model => player.Name == model.Name);
         player.ToggleSpectating(true);
-        if (exist)
-        {
-            loginSystem.Login(player);
-        }
-        else
-        {
-            await signUpSystem.SignUp(player);
-        }
+        await using var context = await contextFactory.CreateDbContextAsync().ConfigureAwait(false);
+        var exist = await context.Characters.AnyAsync(model => player.Name == model.Name);
+        await authenticatedEvent.InvokeAsync(player, exist);
     }
 }
