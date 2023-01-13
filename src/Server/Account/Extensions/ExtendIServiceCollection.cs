87b2fff7beca66c3bc9f211dbc31e5664582d404
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-14 05:00:22 +0700

    refactor: use AddSystem

--- src/Server/Account/Extensions/ExtendIServiceCollection.cs
+++ src/Server/Account/Extensions/ExtendIServiceCollection.cs
@@ -1,6 +1,7 @@
 using Server.Account.Systems.Authentication;
 using Server.Account.Systems.Login;
 using Server.Account.Systems.SignUp;
+using SampSharp.Entities;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -9,8 +10,9 @@ public static partial class ExtendIServiceCollection
     public static IServiceCollection WithAccount(this IServiceCollection self)
     {
         return self
-            .AddSingleton<LoginSystem>()
-            .AddSingleton<SignUpSystem>()
+            .AddSystem<AuthenticationSytem>()
+            .AddSystem<LoginSystem>()
+            .AddSystem<SignUpSystem>()
             .AddSingleton<IAuthenticatedEvent, AuthenticatedEvent>()
             .AddSingleton<ISignedUpEvent, SignedUpEvent>()
             .AddSingleton<ILoginEvent, LoginEvent>();
