a7beb03b321673102e2dbb9341efe85c0d90ba50
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-13 14:54:28 +0700

    perf: optimize message model creation

--- src/Server/Chat/Services/ChatService.cs
+++ src/Server/Chat/Services/ChatService.cs
@@ -16,67 +16,82 @@ public sealed class ChatService : IChatService
         this.entityManager = entityManager;
 	}
 
-	private static void SendMessage(Player player, Color color, string message)
-	{
-		player.SendClientMessage(color, message);
-	}
+    private static void SendMessage(Player player, Color color, string text)
+    {
+        player.SendClientMessage(color, text);
+    }
+
+    private static void SendMessage(Player player, ChatMessageModel model)
+    {
+        player.SendClientMessage(model.Color, model.Text);
+    }
+
+    private static void SendMessages(Player player, ChatMessageModel[] models)
+    {
+        foreach (var model in models)
+        {
+            SendMessage(player, model);
+        }
+    }
+
+    private static void SendInlineMessages(Player player, ChatMessageModel[] models)
+    {
+        var stringBuilder = new StringBuilder(144);
+        foreach (var model in models)
+        {
+            stringBuilder.AppendFormat("{{{0}}}{1} ", model.Color.ToString(), model.Text);
+        }
+        stringBuilder.Remove(stringBuilder.Length - 1, 1);
+        SendMessage(player, Color.White, stringBuilder.ToString());
+    }
 
 	public void SendMessage(Player player, Func<IChatMessageModelFactory, ChatMessageModel> messageCreator)
 	{
-		var model = messageCreator(factory);
-		SendMessage(player, model.Color, model.Text);
+        SendMessage(player, messageCreator(factory));
 	}
 
 	public void SendMessages(Player player, Func<IChatMessageModelFactory, ChatMessageModel[]> messageCreator)
 	{
-		var models = messageCreator(factory);
-		foreach (var model in models)
-		{
-			SendMessage(player, model.Color, model.Text);
-		}
+        SendMessages(player, messageCreator(factory));
 	}
 
 	public void SendInlineMessages(Player player, Func<IChatMessageModelFactory, ChatMessageModel[]> messageCreator)
 	{
-		var models = messageCreator(factory);
-		var stringBuilder = new StringBuilder(144);
-		foreach (var model in models)
-		{
-			stringBuilder.AppendFormat("{{{0}}}{1} ", model.Color.ToString(), model.Text);
-		}
-		stringBuilder.Remove(stringBuilder.Length - 1, 1);
-		SendMessage(player, Color.White, stringBuilder.ToString());
+        SendInlineMessages(player, messageCreator(factory));
 	}
 
     public void SendMessage(Predicate<Player> filter, Func<IChatMessageModelFactory, ChatMessageModel> messageCreator)
     {
+        var model = messageCreator(factory);
         foreach(var i in entityManager.GetComponents<Player>())
         {
             if (filter(i))
             {
-                SendMessage(i, messageCreator);
+                SendMessage(i, model);
             }
         }
     }
 
     public void SendMessages(Predicate<Player> filter, Func<IChatMessageModelFactory, ChatMessageModel[]> messageCreator)
     {
+        var model = messageCreator(factory);
         foreach(var i in entityManager.GetComponents<Player>())
         {
             if (filter(i))
             {
-                SendMessages(i, messageCreator);
+                SendMessages(i, model);
             }
         }
     }
 
     public void SendInlineMessages(Predicate<Player> filter, Func<IChatMessageModelFactory, ChatMessageModel[]> messageCreator)
     {
+        var model = messageCreator(factory);
         foreach(var i in entityManager.GetComponents<Player>())
         {
             if (filter(i))
             {
-                SendInlineMessages(i, messageCreator);
+                SendInlineMessages(i, model);
             }
         }
     }
