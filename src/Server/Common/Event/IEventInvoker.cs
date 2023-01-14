5c03ce7d4d56d15729725720b69b2d36d6013559
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-15 00:56:04 +0700

    feat: implement event invoker and synchronized event invoker

---
+++ src/Server/Common/Event/IEventInvoker.cs
@@ -0,0 +1,8 @@
+namespace Server.Common.Event;
+
+public interface IEventInvoker
+{
+    void Invoke(Delegate @delegate, params object?[]? args);
+    Task InvokeAsync(Delegate @delegate, params object?[]? args);
+    Task InvokeAsyncSerial(Delegate @delegate, params object?[]? args);
+}
