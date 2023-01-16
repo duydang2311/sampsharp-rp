561a5a61a920377f07032f2bac6fba5f10b9029c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-16 22:37:46 +0700

    feat: add DoorObjectModel

---
+++ src/Server/Door/Models/DoorObjectModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Door.Models;
+
+public sealed class DoorObjectModel
+{
+    [Key]
+    public long Id { get; set; }
+    [DefaultValue(0.0f)]
+    public float X { get; set; }
+    [DefaultValue(0.0f)]
+    public float Y { get; set; }
+    [DefaultValue(0.0f)]
+    public float Z { get; set; }
+    [DefaultValue(0.0f)]
+    public float A { get; set; }
+    [DefaultValue(0)]
+    public int Interior { get; set; }
+    [DefaultValue(0)]
+    public int World { get; set; }
+}
