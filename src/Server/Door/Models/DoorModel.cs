556e000533001a10ce633e53df0f9055976ac64b
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-19 22:04:48 +0700

    feat(DoorModel): add DoorObject navigation property

--- src/Server/Door/Models/DoorModel.cs
+++ src/Server/Door/Models/DoorModel.cs
@@ -6,31 +6,32 @@ namespace Server.Door.Models;
 
 public sealed class DoorModel
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public long Id { get; set; }
-    [DefaultValue(0.0f)]
-    public float EntranceX { get; set; }
-    [DefaultValue(0.0f)]
-    public float EntranceY { get; set; }
-    [DefaultValue(0.0f)]
-    public float EntranceZ { get; set; }
-    [DefaultValue(0.0f)]
-    public float EntranceA { get; set; }
-    [DefaultValue(0)]
-    public int EntranceInterior { get; set; }
-    [DefaultValue(0)]
-    public int EntranceWorld { get; set; }
-    [DefaultValue(0.0f)]
-    public float ExitX { get; set; }
-    [DefaultValue(0.0f)]
-    public float ExitY { get; set; }
-    [DefaultValue(0.0f)]
-    public float ExitZ { get; set; }
-    [DefaultValue(0.0f)]
-    public float ExitA { get; set; }
-    [DefaultValue(0)]
-    public int ExitInterior { get; set; }
-    [DefaultValue(0)]
-    public int ExitWorld { get; set; }
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public long Id { get; set; }
+	[DefaultValue(0.0f)]
+	public float EntranceX { get; set; }
+	[DefaultValue(0.0f)]
+	public float EntranceY { get; set; }
+	[DefaultValue(0.0f)]
+	public float EntranceZ { get; set; }
+	[DefaultValue(0.0f)]
+	public float EntranceA { get; set; }
+	[DefaultValue(0)]
+	public int EntranceInterior { get; set; }
+	[DefaultValue(0)]
+	public int EntranceWorld { get; set; }
+	[DefaultValue(0.0f)]
+	public float ExitX { get; set; }
+	[DefaultValue(0.0f)]
+	public float ExitY { get; set; }
+	[DefaultValue(0.0f)]
+	public float ExitZ { get; set; }
+	[DefaultValue(0.0f)]
+	public float ExitA { get; set; }
+	[DefaultValue(0)]
+	public int ExitInterior { get; set; }
+	[DefaultValue(0)]
+	public int ExitWorld { get; set; }
+	public DoorObjectModel? ObjectModel { get; set; }
 }
