73112a522ad3c459e38416d03b41e012c067a322
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-19 23:59:11 +0700

    feat: add door service collection extensions

---
+++ src/Server/Door/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class IServiceCollectionExtensions
+{
+	public static IServiceCollection WithDoor(this IServiceCollection self)
+	{
+		return self;
+	}
+}
--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -5,31 +5,32 @@ using SampSharp.Entities.SAMP;
 
 namespace Server
 {
-    public class Startup : IStartup
-    {
-        public void Configure(IServiceCollection services)
-        {
-            services
-                .WithDatabase()
-                .WithLogging()
-                .WithChat()
-                .WithAccount()
-                .WithI18N((globalOptions, textLocalizerOptions) =>
-                {
-                    globalOptions.AddCulture("vi");
-                    globalOptions.AddCulture("vi-VN");
-                    textLocalizerOptions.BaseName = "Server.Resources.Text";
-                    textLocalizerOptions.DefaultCulture = CultureInfo.GetCultureInfo("vi");
-                })
-                .WithSAMP()
-                .WithCommon();
-        }
+	public class Startup : IStartup
+	{
+		public void Configure(IServiceCollection services)
+		{
+			_ = services
+				.WithDatabase()
+				.WithLogging()
+				.WithChat()
+				.WithAccount()
+				.WithI18N((globalOptions, textLocalizerOptions) =>
+				{
+					globalOptions.AddCulture("vi");
+					globalOptions.AddCulture("vi-VN");
+					textLocalizerOptions.BaseName = "Server.Resources.Text";
+					textLocalizerOptions.DefaultCulture = CultureInfo.GetCultureInfo("vi");
+				})
+				.WithSAMP()
+				.WithCommon()
+				.WithDoor();
+		}
 
-        public void Configure(IEcsBuilder builder)
-        {
-            builder
-                .UseChatMiddlewares()
-                .EnableSampEvents();
-        }
-    }
+		public void Configure(IEcsBuilder builder)
+		{
+			_ = builder
+				.UseChatMiddlewares()
+				.EnableSampEvents();
+		}
+	}
 }
