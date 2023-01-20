ba68efaa1a4e87c836da6d93782309e3168db3b8
NNQA <anhla12h@gmail.com>
2023-01-20 16:53:11 +0700

    feat: repair /me commands

--- src/Server/Character/Systems/RolePlayCommands/MeCommandSystem.cs
+++ src/Server/Character/Systems/RolePlayCommands/MeCommandSystem.cs
@@ -3,17 +3,23 @@ using SampSharp.Entities.SAMP;
 using Server.Chat.Components;
 using Server.Chat.Models;
 using Server.Chat.Services;
+
 using Server.Common.Colors;
+
+using Server.I18N.Localization.Services;
 
 namespace Server.Character.Systems.RolePlayCommands;
 
 public sealed class MeCommandSystem : ISystem
 {
 	private readonly IChatService chatService;
+	private readonly ITextLocalizerService textLocalizerService;
 
-	public MeCommandSystem(ICommandService commandService, IChatService chatService)
+	public MeCommandSystem(ICommandService commandService, IChatService chatService,
+		ITextLocalizerService textLocalizerService)
 	{
 		this.chatService = chatService;
+		this.textLocalizerService = textLocalizerService;
 		commandService.RegisterCommand((model) =>
 		{
 			model.Name = "me";
@@ -25,15 +31,16 @@ public sealed class MeCommandSystem : ISystem
 	public void MeCommand(Player player, string text)
 	{
 		chatService.SendMessage(
+
 			p  => (Vector3.Distance(p.Position, player.Position) < 15f * 15f),
 			a => a.Add(SemanticColor.Roleplay, model => model.MeCommandText, player.Name, text)
 		);
+
 	}
 
-// 123 gh
+
 	public void HelpMeCommand(Player player)
 	{
-//		chatService.SendMessage(
-//			player, factory => factory.Create(Color.FromInteger(0xC2A2DA, ColorFormat.RGB), "Usage: /me [action]."));
+
 	}
 }
