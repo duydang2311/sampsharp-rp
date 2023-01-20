d1793c7e27ec75dd63ca81c5e6790203ea9ca555
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-20 14:31:13 +0700

    feat(Door): register door factory service

--- src/Server/Door/Extensions/IServiceCollectionExtensions.cs
+++ src/Server/Door/Extensions/IServiceCollectionExtensions.cs
@@ -1,9 +1,12 @@
+using Server.Door.Services;
+
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static partial class IServiceCollectionExtensions
 {
 	public static IServiceCollection WithDoor(this IServiceCollection self)
 	{
-		return self;
+		return self
+			.AddSingleton<IDoorFactory, DoorFactory>();
 	}
 }
