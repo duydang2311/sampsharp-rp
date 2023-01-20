3f4e7da54f45fd0b95e84e91ac25b7d2a707dcf8
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-20 16:58:25 +0700

    feat(Common): add semantic colors

---
+++ src/Server/Common/Colors/SemanticColor.cs
@@ -0,0 +1,11 @@
+using SampSharp.Entities.SAMP;
+
+namespace Server.Common.Colors;
+
+public static class SemanticColor
+{
+	public static Color Roleplay => new(0xC2, 0xA2, 0xDA);
+	public static Color Info => new(0, 0, 0xFF);
+	public static Color Success => new(0, 0xFF, 0);
+	public static Color Error => new(0xFF, 0, 0);
+}
