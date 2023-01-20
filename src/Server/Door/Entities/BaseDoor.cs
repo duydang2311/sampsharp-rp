1934ad39248e02ee0d381a39e331c3e61680e5be
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-20 14:30:22 +0700

    feat(Door): implement base door

---
+++ src/Server/Door/Entities/BaseDoor.cs
@@ -0,0 +1,6 @@
+namespace Server.Door.Entities;
+
+public abstract class BaseDoor : IDoor
+{
+	public long Id { get; set; }
+}
---
+++ src/Server/Door/Entities/IDoor.cs
@@ -0,0 +1,6 @@
+namespace Server.Door.Entities;
+
+public interface IDoor
+{
+	long Id { get; set; }
+}
