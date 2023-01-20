d66d1458d2335fbda31831e060ab7b9b8fd468f6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-20 17:29:30 +0700

    feat(DoorCommand): add door command base

---
+++ src/Server/Door/Systems/DoorCommand/DoorCommandSystem.cs
@@ -0,0 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using SampSharp.Entities;
+using SampSharp.Entities.SAMP;
+using Server.Chat.Components;
+using Server.Chat.Services;
+using Server.Common.Colors;
+using Server.Database;
+using Server.Door.Services;
+
+namespace Server.Door.Systems.DoorCommand;
+
+public sealed partial class DoorCommandSystem : ISystem
+{
+	private readonly IArgumentParser argumentParser;
+	private readonly IChatService chatService;
+	private readonly IDbContextFactory<ServerDbContext> dbContextFactory;
+	private readonly DoorFactory doorFactory;
+
+	public DoorCommandSystem(ICommandService commandService, IChatService chatService, IArgumentParser argumentParser, IDbContextFactory<ServerDbContext> dbContextFactory, DoorFactory doorFactory)
+	{
+		this.chatService = chatService;
+		this.argumentParser = argumentParser;
+		this.dbContextFactory = dbContextFactory;
+		this.doorFactory = doorFactory;
+
+		commandService.RegisterCommand(m =>
+		{
+			m.Name = "door";
+			m.Delegate = DoorCommand;
+			m.PermissionLevel = PermissionLevel.Admin;
+		});
+		commandService.RegisterHelper("door", HelpDoorCommand);
+	}
+
+	private void HelpDoorCommand(Player player)
+	{
+		chatService.SendMessage(player, SemanticColor.Info, m => m.DoorCommand_Help);
+		chatService.SendMessage(player, Color.Gray, m => m.DoorCommand_Options);
+	}
+
+	private void DoorCommand(Player player, string argument)
+	{
+		var (option, rest, _) = argument.Split(' ', 2);
+		switch (option)
+		{
+			case "create":
+				{
+					break;
+				}
+			case "destroy":
+				{
+					break;
+				}
+			case "nearby":
+				{
+					break;
+				}
+			default:
+				{
+					HelpDoorCommand(player);
+					break;
+				}
+		}
+	}
+}
