658a80694090bfb0418fe8a64b150837bcf296b4
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-21 10:18:03 +0700

    feat(Door): add door command destroy texts

--- src/Server/I18N/Localization/Models/ITextNameFakeModel.cs
+++ src/Server/I18N/Localization/Models/ITextNameFakeModel.cs
@@ -12,4 +12,8 @@ public interface ITextNameFakeModel
 	object DoorCommand_Create_Options { get; }
 	object DoorCommand_Create_NoEffect { get; }
 	object DoorCommand_Create_Success { get; }
+	object DoorCommand_Destroy_Help { get; }
+	object DoorCommand_Destroy_InvalidId { get; }
+	object DoorCommand_Destroy_SuccessNoEffect { get; }
+	object DoorCommand_Destroy_Success { get; }
 }
