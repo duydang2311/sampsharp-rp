e9589c4d48b7f782606f973a1a100bf08466429b
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-21 11:25:42 +0700

    refactor(Chat): remove sealed keyword from ChatMessageModel

--- src/Server/Chat/Models/ChatMessageModel.cs
+++ src/Server/Chat/Models/ChatMessageModel.cs
@@ -2,7 +2,7 @@ using SampSharp.Entities.SAMP;
 
 namespace Server.Chat.Models;
 
-public sealed class ChatMessageModel
+public class ChatMessageModel
 {
 	public string Text { get; set; } = string.Empty;
 	public Color Color { get; set; } = Color.White;
