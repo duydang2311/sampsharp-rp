fe49e15c18954281d24c69756b9fde6857f718d2
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-23 12:39:52 +0700

    feat(Door): register door command system

--- src/Server/Door/Extensions/IServiceCollectionExtensions.cs
+++ src/Server/Door/Extensions/IServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
+using SampSharp.Entities;
 using Server.Door.Services;
+using Server.Door.Systems.DoorCommand;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,7 @@ public static partial class IServiceCollectionExtensions
 	public static IServiceCollection WithDoor(this IServiceCollection self)
 	{
 		return self
+			.AddSystem<DoorCommandSystem>()
 			.AddSingleton<IDoorFactory, DoorFactory>();
 	}
 }
