3b1f5ff2fe1cc92ad5ccbca2537a574f63ee226c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-24 15:27:06 +0700

    feat(Character): enable systems; register system events

---
+++ src/Server/Character/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+using SampSharp.Entities;
+using Server.Character.Systems.EnterCommand;
+using Server.Character.Systems.ExitCommand;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class IServiceCollectionExtensions
+{
+	public static IServiceCollection WithCharacter(this IServiceCollection self)
+	{
+		return self
+			.AddSystem<EnterCommandSystem>()
+			.AddSystem<ExitCommandSystem>()
+			.AddSingleton<IEnterCommandEvent, EnterCommandEvent>()
+			.AddSingleton<IExitCommandEvent, ExitCommandEvent>();
+	}
+}
