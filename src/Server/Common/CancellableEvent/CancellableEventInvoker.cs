7828aa455e6f9925aa82710231f46615576db630
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-25 21:43:11 +0700

    feat(Common): add CancellableEventInvoker implementation

---
+++ src/Server/Common/CancellableEvent/CancellableEventInvoker.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel;
+
+namespace Server.Common.CancellableEvent;
+
+public class CancellableEventInvoker : ICancellableEventInvoker
+{
+	private object?[] CreateExtendedArgs(object?[]? args, object extendedValue)
+	{
+		if (args is null)
+		{
+			return new object?[] { extendedValue };
+		}
+		var extendedArgs = new object?[args.Length + 1];
+		args.CopyTo(extendedArgs, 0);
+		extendedArgs[extendedArgs.Length - 1] = extendedValue;
+		return extendedArgs;
+	}
+
+	public void Invoke(Delegate @delegate, params object?[]? args)
+	{
+		var cancelEventArgs = new CancelEventArgs();
+		object?[] extendedArgs = CreateExtendedArgs(args, cancelEventArgs);
+		foreach (var invocation in @delegate.GetInvocationList())
+		{
+			invocation.Method.Invoke(invocation.Target, extendedArgs);
+			if (cancelEventArgs.Cancel)
+			{
+				break;
+			}
+		}
+	}
+
+	public Task InvokeAsync(Delegate @delegate, params object?[]? args)
+	{
+		return InvokeAsyncSerial(@delegate, args);
+	}
+
+	public async Task InvokeAsyncSerial(Delegate @delegate, params object?[]? args)
+	{
+		var cancelEventArgs = new CancelEventArgs();
+		object?[] extendedArgs = CreateExtendedArgs(args, cancelEventArgs);
+		foreach (var invocation in @delegate.GetInvocationList())
+		{
+			await (Task)invocation.Method.Invoke(invocation.Target, args)!;
+			if (cancelEventArgs.Cancel)
+			{
+				break;
+			}
+		}
+	}
+}
