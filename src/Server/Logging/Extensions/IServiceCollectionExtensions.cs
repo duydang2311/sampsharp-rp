63241772781b92883e207fa8952e2bb9b8d94d76
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-25 11:59:53 +0700

    feat(Logging): use serilog

--- src/Server/Logging/Extensions/IServiceCollectionExtensions.cs
+++ src/Server/Logging/Extensions/IServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Templates;
+using Serilog.Templates.Themes;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -6,10 +9,25 @@ public static partial class IServiceCollectionExtensions
 {
 	public static IServiceCollection WithLogging(this IServiceCollection self)
 	{
-		return self.AddLogging(configure =>
+		return self.AddLogging(logging =>
 		{
-			configure.AddConsole();
-			configure.SetMinimumLevel(LogLevel.Information);
+			var template = "{@l:w4}:" +
+				"{#if SourceContext is not null} {Substring(SourceContext, IndexOf(SourceContext, '.') + 1)}[{@i}]{#end} ({@t:HH:mm:ss})\n" +
+				"{#if Length(Scope) > 0}" +
+				"      {#each s in Scope}=> {s}{#delimit} {#end}\n" +
+				"{#end}" +
+				"      {@m}\n" +
+				"{@x}";
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Information()
+				.Enrich.FromLogContext()
+				.WriteTo.Console(new ExpressionTemplate(template, theme: TemplateTheme.Code))
+				.WriteTo.File(new ExpressionTemplate(template), "logs/server-.txt", rollingInterval: RollingInterval.Day)
+				.CreateLogger();
+
+			logging.ClearProviders();
+			logging.AddSerilog(dispose: true);
+			logging.SetMinimumLevel(LogLevel.Information);
 		});
 	}
 }
