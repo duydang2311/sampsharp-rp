38c6fd67c5235a8e765fda5bd7db9a985ef0332c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-01-31 13:12:11 +0700

    feat(SpatialGrid): implement Cell

---
+++ src/Server/SpatialGrid/ICell.cs
@@ -0,0 +1,11 @@
+﻿using SampSharp.Entities;
+
+namespace Server.SpatialGrid;
+
+public interface ICell : IBaseCell
+{
+	IEnumerable<Component> Components { get; }
+	void Add(Component component);
+	bool Remove(Component component);
+	void Clear();
+}
