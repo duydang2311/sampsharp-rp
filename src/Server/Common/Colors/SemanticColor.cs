c0f8ca95e7c7166195d8f160f334903a01b396be
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-02-06 16:52:10 +0700

    feat(SemanticColor): add several colors

--- src/Server/Common/Colors/SemanticColor.cs
+++ src/Server/Common/Colors/SemanticColor.cs
@@ -6,8 +6,12 @@ public static class SemanticColor
 {
 	public static Color Roleplay => new(0xC2, 0xA2, 0xDA);
 	public static Color Info => new(0, 0, 0xFF);
-	public static Color Success => new(0, 0xFF, 0);
-	public static Color Error => new(0xFF, 0, 0);
-	public static Color Neutral => Color.White;
-	public static Color LowAttention => Color.SlateGray;
+	public static Color Success => new(34, 197, 94);
+	public static Color Error => new(239, 68, 68);
+	public static Color Warning => new(234, 179, 8);
+	public static Color Neutral => new(203, 213, 225);
+	public static Color LowAttention => new(107, 114, 128);
+	public static Color Help => new(0x27, 0xBE, 0xF0);
+	public static Color System => new(163, 163, 163);
+	public static Color ChatMessage => new(241, 245, 249);
 }
