f49815766e31d45a229255f8344299c1abee76fd
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-02-06 16:51:38 +0700

    feat(I18N): add several badges

--- src/Server/I18N/Localization/Models/ILocalizedBadge.cs
+++ src/Server/I18N/Localization/Models/ILocalizedBadge.cs
@@ -3,5 +3,8 @@ namespace Server.I18N.Localization.Models;
 public interface ILocalizedBadge
 {
 	object Badge_System { get; }
-	object Badge_CommandUsage { get; }
+	object Badge_Help { get; }
+	object Badge_Success { get; }
+	object Badge_Warning { get; }
+	object Badge_Error { get; }
 }
