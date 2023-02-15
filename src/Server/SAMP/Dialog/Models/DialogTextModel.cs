6c6fcea16426b9f0f4d115ed7ed3d68a04928d0d
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-02-15 19:16:25 +0700

    feat(Dialog): add dialog text models

---
+++ src/Server/SAMP/Dialog/Models/DialogTextModel.cs
@@ -0,0 +1,7 @@
+namespace Server.SAMP.Dialog.Models;
+
+public class DialogTextModel
+{
+	public string Text { get; set; } = string.Empty;
+	public object[] Args { get; set; } = Array.Empty<object>();
+}
---
+++ src/Server/SAMP/Dialog/Models/TaggedDialogTextModel.cs
@@ -0,0 +1,6 @@
+namespace Server.SAMP.Dialog.Models;
+
+public class TaggedDialogTextModel : DialogTextModel
+{
+	public object? Tag { get; set; }
+}
