9b234539b931a2fa87076af2901a72c1ab8fdd01
NNQA <anhla12h@gmail.com>
2023-02-16 15:33:54 +0700

    feat: refactor /do command help

--- src/Server/Character/Systems/RolePlayCommands/DoCommandSystem.cs
+++ src/Server/Character/Systems/RolePlayCommands/DoCommandSystem.cs
@@ -24,13 +24,17 @@ public sealed class DoCommandSystem : ISystem
 	public void DoCommand(Player player, string text)
 	{
 		chatService.SendMessage(
-			p => (Vector3.DistanceSquared(p.Position, player.Position) <= 15f* 15f),
+			p => (Vector3.DistanceSquared(p.Position, player.Position) <= 15f * 15f),
 			b => b.Add((SemanticColor.Roleplay), ex => ex.DoCommandText, player.Name, text)
-		);	
+		);
 	}
 
 	public void DoCommandHelp(Player player)
 	{
-		
+		chatService.SendMessage(
+			player,
+		 	b => b.Add((SemanticColor.Roleplay), model => model.DoCommandHelp,
+				"Usega: [status] ((Character_Name))")
+		);
 	}
 }
