685cd8cf8ea9da5441657adce66ad2281114fa29
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-02-20 00:25:37 +0700

    test(Geometry): use area factory

--- src/Server.Tests/Geometry/TestCircleArea.cs
+++ src/Server.Tests/Geometry/TestCircleArea.cs
@@ -2,29 +2,38 @@ using FluentAssertions;
 using FluentAssertions.Execution;
 using SampSharp.Entities.SAMP;
 using Server.Geometry.Entities;
+using Server.Geometry.Services;
 
 namespace Server.Tests.Geometry;
 
 public class TestCircleArea
 {
+	private IAreaFactory areaFactory = null!;
+
+	[SetUp]
+	public void SetUp()
+	{
+		areaFactory = new AreaFactory();
+	}
+
 	[Test]
 	public void Circle_ContainsPoint()
 	{
-		IArea circle = new CircleArea(2, 0) { Radius = 2 };
+		IArea circle = areaFactory.CreateCircle(2, 0, 2);
 		circle.Contains(new Vector2(0, 0)).Should().BeTrue();
 	}
 
 	[Test]
 	public void Circle_Not_ContainsPoint()
 	{
-		IArea circle = new CircleArea(2, 0) { Radius = 2 };
+		IArea circle = areaFactory.CreateCircle(2, 0, 2);
 		circle.Contains(new Vector2(0, 1)).Should().BeFalse();
 	}
 
 	[Test]
 	public void Circle_OverlapsArea()
 	{
-		IArea circle = new CircleArea(2, 0) { Radius = 2 };
+		IArea circle = areaFactory.CreateCircle(2, 0, 2);
 		IArea rectangle1 = new RectangleArea(new Vector2(0, 6), new Vector2(6, 2));
 		IArea rectangle2 = new RectangleArea(new Vector2(-100, 100), new Vector2(100, -100));
 		IArea rectangle3 = new RectangleArea(new Vector2(1, 2), new Vector2(3, 1));
@@ -41,7 +50,7 @@ public class TestCircleArea
 	[Test]
 	public void Circle_Not_OverlapsArea()
 	{
-		IArea circle = new CircleArea(2, 0) { Radius = 2 };
+		IArea circle = areaFactory.CreateCircle(2, 0, 2);
 		IArea rectangle1 = new RectangleArea(new Vector2(0, 6), new Vector2(6, 2.00001));
 		IArea rectangle2 = new RectangleArea(new Vector2(-100, 100), new Vector2(-0.0001, 0001));
 		IArea rectangle3 = new RectangleArea(new Vector2(4.00001, 6), new Vector2(6, 0));
--- src/Server.Tests/Geometry/TestPolygonArea.cs
+++ src/Server.Tests/Geometry/TestPolygonArea.cs
@@ -2,22 +2,25 @@ using FluentAssertions;
 using FluentAssertions.Execution;
 using SampSharp.Entities.SAMP;
 using Server.Geometry.Entities;
+using Server.Geometry.Services;
 
 namespace Server.Tests.Geometry;
 
 public class TestPolygonArea
 {
-	IPolygonArea polygon = null!;
+	private IAreaFactory areaFactory = null!;
+	private IPolygonArea polygon = null!;
 
 	[SetUp]
 	public void SetUp()
 	{
-		polygon = new PolygonArea();
-		polygon.Add(new Vector2(0, 0));
-		polygon.Add(new Vector2(5, 0));
-		polygon.Add(new Vector2(5, 5));
-		polygon.Add(new Vector2(2.5, 2.5));
-		polygon.Add(new Vector2(0, 7.5));
+		areaFactory = new AreaFactory();
+		polygon = areaFactory.CreatePolygon(
+			new Vector2(0, 0),
+			new Vector2(5, 0),
+			new Vector2(5, 5),
+			new Vector2(2.5, 2.5),
+			new Vector2(0, 7.5));
 	}
 	[Test]
 	public void Polygon_ContainsPoint()
@@ -52,14 +55,14 @@ public class TestPolygonArea
 	[Test]
 	public void Polygon_OverlapsArea()
 	{
-		IArea rectangle1 = new RectangleArea(new Vector2(4.99, 4.99), new Vector2(7, 0));
-		IArea rectangle2 = new RectangleArea(new Vector2(-100, 100), new Vector2(0.001, 0.001));
-		IArea rectangle3 = new RectangleArea(new Vector2(0, 10), new Vector2(2, 7.499));
-		IArea rectangle4 = new RectangleArea(new Vector2(2.4, 2.6), new Vector2(2.6, 2.4));
-		IArea circle1 = new CircleArea(-2, 0) { Radius = 2 };
-		IArea circle2 = new CircleArea(0, 9.5) { Radius = 2 };
-		IArea circle3 = new CircleArea(7, 0) { Radius = 2 };
-		IArea circle4 = new CircleArea(2.5, 5.5) { Radius = 2 };
+		IArea rectangle1 = areaFactory.CreateRectangle(new Vector2(4.99, 4.99), new Vector2(7, 0));
+		IArea rectangle2 = areaFactory.CreateRectangle(new Vector2(-100, 100), new Vector2(0.001, 0.001));
+		IArea rectangle3 = areaFactory.CreateRectangle(new Vector2(0, 10), new Vector2(2, 7.499));
+		IArea rectangle4 = areaFactory.CreateRectangle(new Vector2(2.4, 2.6), new Vector2(2.6, 2.4));
+		IArea circle1 = areaFactory.CreateCircle(-2, 0, 2);
+		IArea circle2 = areaFactory.CreateCircle(0, 9.5f, 2);
+		IArea circle3 = areaFactory.CreateCircle(7, 0, 2);
+		IArea circle4 = areaFactory.CreateCircle(2.5f, 5.5f, 2);
 		using (new AssertionScope())
 		{
 			polygon.Overlaps(rectangle1).Should().BeTrue();
@@ -76,14 +79,14 @@ public class TestPolygonArea
 	[Test]
 	public void Polygon_Not_OverlapsArea()
 	{
-		IArea rectangle1 = new RectangleArea(new Vector2(5.01, 5.01), new Vector2(7, 0));
-		IArea rectangle2 = new RectangleArea(new Vector2(-100, 100), new Vector2(-0.001, -0.001));
-		IArea rectangle3 = new RectangleArea(new Vector2(0, 10), new Vector2(2, 7.5001));
-		IArea rectangle4 = new RectangleArea(new Vector2(2.5, 3), new Vector2(2.51, 2.6));
-		IArea circle1 = new CircleArea(-2, 0) { Radius = 1.99f };
-		IArea circle2 = new CircleArea(0, 9.5) { Radius = 1.99f };
-		IArea circle3 = new CircleArea(7, 0) { Radius = 1.99f };
-		IArea circle4 = new CircleArea(2.5, 7) { Radius = 2 };
+		IArea rectangle1 = areaFactory.CreateRectangle(new Vector2(5.01, 5.01), new Vector2(7, 0));
+		IArea rectangle2 = areaFactory.CreateRectangle(new Vector2(-100, 100), new Vector2(-0.001, -0.001));
+		IArea rectangle3 = areaFactory.CreateRectangle(new Vector2(0, 10), new Vector2(2, 7.5001));
+		IArea rectangle4 = areaFactory.CreateRectangle(new Vector2(2.5, 3), new Vector2(2.51, 2.6));
+		IArea circle1 = areaFactory.CreateCircle(-2, 0, 1.99f);
+		IArea circle2 = areaFactory.CreateCircle(0, 9.5f, 1.99f);
+		IArea circle3 = areaFactory.CreateCircle(7, 0, 1.99f);
+		IArea circle4 = areaFactory.CreateCircle(2.5f, 7, 2);
 		using (new AssertionScope())
 		{
 			polygon.Overlaps(rectangle1).Should().BeFalse();
