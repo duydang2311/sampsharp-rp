b1eb12b7fa13008952430d48d77bf3a5c857dfc2
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-02-19 23:04:30 +0700

    test(SpatialGrid): test spatial grid Add and Remove

--- src/Server.Tests/SpatialGrid/TestSpatialGrid.cs
+++ src/Server.Tests/SpatialGrid/TestSpatialGrid.cs
@@ -1,6 +1,7 @@
 using FluentAssertions;
 using FluentAssertions.Execution;
-using SampSharp.Entities.SAMP;
+using Server.Geometry.Entities;
+using Server.SpatialGrid.Components;
 using Server.SpatialGrid.Entities;
 using Server.SpatialGrid.Services;
 
@@ -8,84 +9,16 @@ namespace Server.Tests.SpatialGrid;
 
 public class TestSpatialGrid
 {
-	[Test]
-	public void Grid_HasCorrectCellSize()
+	private IGrid grid = null!;
+
+	[SetUp]
+	public void SetUp()
 	{
-		var grid = new GridBuilder()
-			.SetTop(-3000)
+		grid = new GridBuilder()
+			.SetTop(3000)
 			.SetLeft(-3000)
 			.SetRight(3000)
-			.SetBottom(3000)
-			.SetRows(12)
-			.SetColumns(10)
-			.BuildGrid();
-		using (new AssertionScope())
-		{
-			grid.CellWidth.Should().Be(6000 / 10);
-			grid.CellHeight.Should().Be(6000 / 12);
-		}
-	}
-
-	[Test]
-	public void Grid_ComputeCorrectly()
-	{
-		var grid = new GridBuilder()
-			.SetTop(-3000)
-			.SetLeft(-3000)
-			.SetRight(3000)
-			.SetBottom(3000)
-			.SetRows(10)
-			.SetColumns(3)
-			.BuildGrid();
-		using (new AssertionScope())
-		{
-			var ok = grid.TryComputeIndex(new Vector2(-3000, -2400), out var row, out var col);
-			ok.Should().BeTrue();
-			row.Should().Be(1);
-			col.Should().Be(0);
-
-			ok = grid.TryComputeIndex(new Vector2(-1000, -1800), out row, out col);
-			ok.Should().BeTrue();
-			row.Should().Be(2);
-			col.Should().Be(1);
-
-			ok = grid.TryComputeIndex(new Vector2(2999, 2999), out row, out col);
-			ok.Should().BeTrue();
-			row.Should().Be(9);
-			col.Should().Be(2);
-		}
-	}
-
-	[Test]
-	public void NestedGrid_HasCorrectCellSize()
-	{
-		var grid = new GridBuilder()
-			.SetTop(-3000)
-			.SetLeft(-3000)
-			.SetRight(3000)
-			.SetBottom(3000)
-			.SetRows(12)
-			.SetColumns(10)
-			.SetAsInnerGrid(0, 0, innerGrid => innerGrid
-				.SetRows(4)
-				.SetColumns(4))
-			.BuildGrid();
-		using (new AssertionScope())
-		{
-			grid.FindCells(new Vector2(-3000, -3000), 0).First().Should().BeAssignableTo<IGrid>();
-			(grid.FindCells(new Vector2(-3000, -3000), 0).First() as IGrid)!.CellWidth.Should().Be(6000 / 10 / 4);
-			(grid.FindCells(new Vector2(-3000, -3000), 0).First() as IGrid)!.CellHeight.Should().Be(6000 / 12 / 4);
-		}
-	}
-
-	[Test]
-	public void NestedGrid_ComputeCorrectly()
-	{
-		var grid = new GridBuilder()
-			.SetTop(-3000)
-			.SetLeft(-3000)
-			.SetRight(3000)
-			.SetBottom(3000)
+			.SetBottom(-3000)
 			.SetRows(12)
 			.SetColumns(10)
 			.SetAsInnerGrid(0, 0, innerGrid => innerGrid
@@ -95,35 +28,27 @@ public class TestSpatialGrid
 				.SetRows(4)
 				.SetColumns(4))
 			.BuildGrid();
+	}
+
+	[Test]
+	public void Grid_AddAndRemove_ReturnsCorrectCellCount()
+	{
+		var smallComponent = new TestComponent(new CircleArea(-2400, 2500) { Radius = 1f });
+		var mediumComponent = new TestComponent(new CircleArea(-2400, 2500) { Radius = 600f });
+		var largeComponent = new TestComponent(new CircleArea(-6000, 6000) { Radius = 6000f });
 		using (new AssertionScope())
 		{
-			var innerGrid1 = (IGrid)grid.FindCells(new Vector2(-3000, -3000), 0).First();
-			var innerGrid2 = (IGrid)grid.FindCells(new Vector2(2400, 2500), 0).First();
-
-			var ok = innerGrid1.TryComputeIndex(new Vector2(-3000, -3000), out var row, out var col);
-			ok.Should().BeTrue();
-			row.Should().Be(0);
-			col.Should().Be(0);
-
-			ok = innerGrid1.TryComputeIndex(new Vector2(-3000 + 150, -3000 + 250), out row, out col);
-			ok.Should().BeTrue();
-			row.Should().Be(2);
-			col.Should().Be(1);
-
-			ok = innerGrid2.TryComputeIndex(new Vector2(2400, 2500), out row, out col);
-			ok.Should().BeTrue();
-			row.Should().Be(0);
-			col.Should().Be(0);
-
-			ok = innerGrid2.TryComputeIndex(new Vector2(2400 + 300, 2500 + 375), out row, out col);
-			ok.Should().BeTrue();
-			row.Should().Be(3);
-			col.Should().Be(2);
-
-			ok = innerGrid2.TryComputeIndex(new Vector2(2999, 2999), out row, out col);
-			ok.Should().BeTrue();
-			row.Should().Be(3);
-			col.Should().Be(3);
+			grid.Add(smallComponent).Should().Be(3);
+			grid.Add(mediumComponent).Should().Be(10);
+			grid.Add(largeComponent).Should().Be(150);
+			grid.Remove(smallComponent).Should().Be(3);
+			grid.Remove(mediumComponent).Should().Be(10);
+			grid.Remove(largeComponent).Should().Be(150);
 		}
+	}
+
+	private class TestComponent : BaseSpatialComponent
+	{
+		public TestComponent(IArea area) : base(area) { }
 	}
 }
