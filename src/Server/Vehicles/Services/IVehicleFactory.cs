8d9d5565dd19a464b694237ff1d0f429454a67e2
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-02-25 01:20:01 +0700

    feat(VehicleFactory): add Get methods

--- src/Server/Vehicles/Services/IVehicleFactory.cs
+++ src/Server/Vehicles/Services/IVehicleFactory.cs
@@ -1,8 +1,14 @@
+using SampSharp.Entities;
 using SampSharp.Entities.SAMP;
+using Server.Vehicles.Components;
 
 namespace Server.Vehicles.Services;
 
 public interface IVehicleFactory
 {
 	Vehicle? CreateVehicle(VehicleModelType type, Vector3 position, float rotation, int color1, int color2, int respawnDelay = -1, bool addSiren = false);
+	Vehicle? GetVehicle(EntityId entityId);
+	Vehicle? GetVehicle(int vehicleid);
+	IReadOnlyCollection<Vehicle> GetVehicles();
+	IReadOnlyCollection<VehicleComponent> GetVehicleComponents();
 }
--- src/Server/Vehicles/Services/VehicleFactory.cs
+++ src/Server/Vehicles/Services/VehicleFactory.cs
@@ -1,15 +1,18 @@
 using SampSharp.Entities;
 using SampSharp.Entities.SAMP;
+using Server.Vehicles.Components;
 
 namespace Server.Vehicles.Services;
 
 public sealed class VehicleFactory : IVehicleFactory
 {
 	private readonly IWorldService worldService;
+	private readonly IEntityManager entityManager;
 
-	public VehicleFactory(IWorldService worldService)
+	public VehicleFactory(IWorldService worldService, IEntityManager entityManager)
 	{
 		this.worldService = worldService;
+		this.entityManager = entityManager;
 	}
 
 	public Vehicle? CreateVehicle(VehicleModelType type, Vector3 position, float rotation, int color1, int color2, int respawnDelay = -1, bool addSiren = false)
@@ -23,5 +26,25 @@ public sealed class VehicleFactory : IVehicleFactory
 		{
 			return default;
 		}
+	}
+
+	public Vehicle? GetVehicle(EntityId entityId)
+	{
+		return entityManager.GetComponent<Vehicle>(entityId);
+	}
+
+	public Vehicle? GetVehicle(int vehicleId)
+	{
+		return GetVehicle(SampEntities.GetVehicleId(vehicleId));
+	}
+
+	public IReadOnlyCollection<Vehicle> GetVehicles()
+	{
+		return entityManager.GetComponents<Vehicle>();
+	}
+
+	public IReadOnlyCollection<VehicleComponent> GetVehicleComponents()
+	{
+		return entityManager.GetComponents<VehicleComponent>();
 	}
 }
