530304c49a1ddf8a9547c48025c62291df825e96
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-02-25 17:28:50 +0700

    refactor(VehicleFactory): use ServerVehicle for creating instance

--- src/Server/Vehicles/Services/IVehicleFactory.cs
+++ src/Server/Vehicles/Services/IVehicleFactory.cs
@@ -1,12 +1,13 @@
 using SampSharp.Entities;
 using SampSharp.Entities.SAMP;
 using Server.Vehicles.Components;
+using Server.Vehicles.Entities;
 
 namespace Server.Vehicles.Services;
 
 public interface IVehicleFactory
 {
-	Vehicle? CreateVehicle(VehicleModelType type, Vector3 position, float rotation, int color1, int color2, int respawnDelay = -1, bool addSiren = false);
+	Vehicle? CreateVehicle(VehicleModelType type, Vector3 position, float rotation, int interior, int world, int color1, int color2, int respawnDelay = -1, bool addSiren = false);
 	Vehicle? GetVehicle(EntityId entityId);
 	Vehicle? GetVehicle(int vehicleid);
 	IReadOnlyCollection<Vehicle> GetVehicles();
--- src/Server/Vehicles/Services/VehicleFactory.cs
+++ src/Server/Vehicles/Services/VehicleFactory.cs
@@ -1,31 +1,43 @@
 using SampSharp.Entities;
 using SampSharp.Entities.SAMP;
 using Server.Vehicles.Components;
+using Server.Vehicles.Entities;
 
 namespace Server.Vehicles.Services;
 
 public sealed class VehicleFactory : IVehicleFactory
 {
-	private readonly IWorldService worldService;
 	private readonly IEntityManager entityManager;
+	private readonly WorldServiceNative worldServiceNative;
 
-	public VehicleFactory(IWorldService worldService, IEntityManager entityManager)
+	public VehicleFactory(IEntityManager entityManager, WorldServiceNative worldServiceNative)
 	{
-		this.worldService = worldService;
 		this.entityManager = entityManager;
+		this.worldServiceNative = worldServiceNative;
 	}
 
-	public Vehicle? CreateVehicle(VehicleModelType type, Vector3 position, float rotation, int color1, int color2, int respawnDelay = -1, bool addSiren = false)
+	public Vehicle? CreateVehicle(VehicleModelType type, Vector3 position, float rotation, int interior, int world, int color1, int color2, int respawnDelay = -1, bool addSiren = false)
 	{
-		try
-		{
-			var vehicle = worldService.CreateVehicle(type, position, rotation, color1, color2, respawnDelay, addSiren);
-			return vehicle;
-		}
-		catch (EntityCreationException)
+		var id = worldServiceNative.CreateVehicle((int)type, position.X, position.Y, position.Z, rotation, color1, color2, respawnDelay, addSiren);
+
+		if (id == NativeVehicle.InvalidId)
 		{
 			return default;
 		}
+		var entity = SampEntities.GetVehicleId(id);
+		entityManager.Create(entity, EntityId.Empty);
+
+		var vehicle = new ServerVehicle()
+		{
+			Interior = interior,
+			VirtualWorld = world,
+			// TODO: reduce 2 native calls here
+			PrimaryColor = color1,
+			SecondaryColor = color2,
+		};
+		entityManager.AddComponent(entity, new NativeVehicle());
+		entityManager.AddComponent<Vehicle>(entity, vehicle);
+		return vehicle;
 	}
 
 	public Vehicle? GetVehicle(EntityId entityId)
